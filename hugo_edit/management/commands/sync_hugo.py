5f437e602f7fb47cbffb83398c5706213680e874
Francisco Vergara <fvergara@drivetech.pro>
2026-07-05 05:45:35 -0400

    feat(hugo): todo el sitio se encuentra en base de datos.

--- hugo_edit/management/commands/sync_hugo.py
+++ hugo_edit/management/commands/sync_hugo.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from hugo_edit.models import Activity
+from hugo_edit.models import Activity, Noticia, Estudio
 import subprocess
 import os
 from django.conf import settings
@@ -9,12 +9,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write('Generando ficheros Markdown desde la Base de Datos...')
-        activities = Activity.objects.all()
-        
-        for act in activities:
+
+        for act in Activity.objects.all():
             act.generate_markdown()
-            self.stdout.write(f'- Creado: {act.title}')
-            
+            self.stdout.write(f'- Actividad: {act.title}')
+
+        for noticia in Noticia.objects.all():
+            noticia.generate_markdown()
+            self.stdout.write(f'- Noticia: {noticia.title}')
+
+        for estudio in Estudio.objects.all():
+            estudio.generate_markdown()
+            self.stdout.write(f'- Estudio: {estudio.title}')
+
         self.stdout.write('Reconstruyendo Hugo site...')
         result = subprocess.run(
             ['hugo', '--minify'],
@@ -22,7 +29,7 @@ class Command(BaseCommand):
             capture_output=True,
             text=True
         )
-        
+
         if result.returncode == 0:
             self.stdout.write(self.style.SUCCESS('Sincronización de Hugo completada exitosamente.'))
         else:
